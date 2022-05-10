67cd078d2fb0d4c355bd60a5d17530a16bccc3da
SergeyKovalenko2012 <sergeykovalenko201290@gmail.com>
2022-05-10 21:45:12 +0300

    library-header/add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './sass/main.scss';
 
 import './js/moviePopular';
+import './js/header-library';
 import './js/modalClose';
 import './js/onScroll';
 import './js/themeMode';
+
 import './js/modal';
 import './js/home';
 import './js/logo';
 import './js/spinner';
-
