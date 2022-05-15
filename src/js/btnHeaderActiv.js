e153c894bf719287e0d3b4ffe69eb450efde5c7f
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2022-05-16 00:18:39 +0300

    Исправлено переключение активных кнопок в heder

--- src/js/btnHeaderActiv.js
+++ src/js/btnHeaderActiv.js
@@ -1,18 +1,26 @@
 const btnHome = document.querySelector('.js-heder-home');
-const btnLibrary = document.querySelector('.js-heder-library');
+const btnLibrary = document.querySelector('.js-library');
 const btnLogo = document.querySelector('.logo');
 
-btnHome.addEventListener('click', btnActivHeader);
-btnLibrary.addEventListener('click', btnActivHeader);
+btnHome.addEventListener('click', btnActivHeaderHome);
+btnLibrary.addEventListener('click', btnActivHeaderLibrsry);
 btnLogo.addEventListener('click', btnActivHeaderLogo);
 
-function btnActivHeader() {
-  btnHome.classList.toggle('nav__list-item--activ');
-  btnLibrary.classList.toggle('nav__list-item--activ');
-}
 function btnActivHeaderLogo() {
-  if (btnLibrary.classList.contains('nav__list-item--activ')) {
-    btnHome.classList.toggle('nav__list-item--activ');
-    btnLibrary.classList.toggle('nav__list-item--activ');
+  if (btnLibrary.classList.contains('nav__list-text--activ')) {
+    btnHome.classList.toggle('nav__list-text--activ');
+    btnLibrary.classList.toggle('nav__list-text--activ');
+  }
+}
+function btnActivHeaderHome() {
+  if (btnLibrary.classList.contains('nav__list-text--activ')) {
+    btnHome.classList.toggle('nav__list-text--activ');
+    btnLibrary.classList.toggle('nav__list-text--activ');
+  }
+}
+function btnActivHeaderLibrsry() {
+  if (btnHome.classList.contains('nav__list-text--activ')) {
+    btnHome.classList.toggle('nav__list-text--activ');
+    btnLibrary.classList.toggle('nav__list-text--activ');
   }
 }
