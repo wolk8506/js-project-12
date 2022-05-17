e1df99013f6d65dbc10a0b2f7db303feeaee1556
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2022-05-17 19:00:27 +0300

    исправление инициализации переменных

--- src/js/initLS.js
+++ src/js/initLS.js
@@ -7,3 +7,13 @@ let movieQueue = JSON.parse(localStorage.getItem('movieQueue'));
 if (movieQueue === null) {
   localStorage.setItem('movieQueue', JSON.stringify([]));
 }
+
+let count = Number(localStorage.getItem('currentPage'));
+if (count === 0) {
+  localStorage.setItem(`currentPage`, `1`);
+}
+
+let pageResetLoad = Number(localStorage.getItem('pageResetLoad'));
+if (pageResetLoad === 0) {
+  localStorage.setItem(`pageResetLoad`, `1`);
+}
